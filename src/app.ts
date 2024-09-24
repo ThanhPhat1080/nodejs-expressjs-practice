e099c11a74ef9ae8452cec7ee6c354da30a0809a
Phat Truong <thanhphat.994@gmail.com>
2024-09-24 23:17:39 +0700

    Update logger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 // Libraries
 import express, { Router } from 'express';
 import { config as dotenvConfig } from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 // Swagger
 import swaggerConfig from './swagger.json';
@@ -18,6 +20,7 @@ import type { Express, NextFunction, Request, Response } from 'express';
 
 // Routers
 import { userRouter, projectRouter } from './routers';
+import logEvents from './helpers/log';
 
 // Config dotenv
 dotenvConfig();
@@ -25,7 +28,6 @@ dotenvConfig();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-console.log("=== process.env.NODE_ENV ===", process.env.NODE_ENV)
 
 const mongodbService = new MongoDbConnection((process.env.MONGO_URI as string) || 'mongodb://0.0.0.0:27017/local');
 mongodbService.connect();
@@ -39,6 +41,12 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: t
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Hidden metadata on header
+app.use(helmet());
+
+// For debug logger
+app.use(morgan('common'));
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
@@ -48,21 +56,25 @@ const appRouters = Router();
 appRouters.use('/user', userRouter);
 appRouters.use('/project', projectRouter);
 
-app.use('/api', appRouters);
+app.use('/api/v1', appRouters);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     next(CreateErrorMiddleware.NotFound('Not found!'));
 });
 
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logEvents(`[path: ${req.url} | method: ${req.method}]\tError --- Status: ${err.status} - ${err.message}`);
+
     res.status(err.status || 500).json({
         status: err.status || 500,
         message: err.message,
     });
 });
 
+
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log("[server]: Server is running on NODE_ENV =", process.env.NODE_ENV);
+    console.log(`[server]: Server is running at PORT: ${port}`);
 });
 
 process.on('SIGINT', async () => {
